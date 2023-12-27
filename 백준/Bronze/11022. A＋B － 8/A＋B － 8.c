5d104995dddba95f5789e2a6593ec254d6adbe25
zxodan <130204907+zxodan@users.noreply.github.com>
2023-12-28 00:00:49 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int n, a, b;
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        scanf("%d %d",&a, &b);
+        printf("Case #%d: %d + %d = %d\n",i+1, a,b,a+b);
+    }
+    return 0;
+}
